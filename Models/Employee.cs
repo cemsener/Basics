f675c9a1820d6745275bab610079a3905f957e58
cemsener <cemsener_@hotmail.com>
2023-08-29 16:58:03 +0300

    mvc_temel_commit

---
+++ Models/Employee.cs
@@ -0,0 +1,13 @@
+using System.Runtime.Intrinsics.X86;
+
+namespace Basics.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string FullName => $"{FirstName} {LastName.ToUpper()}";
+        public int Age { get; set; }
+    }
+}
